78f0a7d3c257f0eb9cf47903e253220f414b0316
mzwing <mzwing@mzwing.eu.org>
2023-04-15 18:16:27 +0800

    try to fix libs 2

--- default.nix
+++ default.nix
@@ -12,14 +12,10 @@ let
   inherit (pkgs) lib;
   overlayList = builtins.attrValues (import ./overlays);
   pkgs' = lib.foldl (prev: prev.extend) pkgs overlayList;
-  selfLib = import ./lib { inherit (pkgs') lib; };
 in
 {
-  lib = selfLib;
   modules = import ./modules;
   overlays = import ./overlays;
 }
   //
-pkgs'.callPackage ./pkgs {
-  inherit selfLib;
-}
+pkgs'.callPackage ./pkgs;
